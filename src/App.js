70f603da5cb7866bcd74a4e1e4afa82bcf8233aa
AdanJaramillo <est2615adan@gmail.com>
2022-02-09 18:16:44 -0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import { HomePage } from "./pages/Home";
+//import { Gallery } from "./components/Gallery";
+//import { SpeedDial } from "./components/SpeedDial";
+
+
+function App() {
+  return (
+    <div className="App">
+     <HomePage></HomePage>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Button/Button.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import {Button} from "@mui/material";
+
+function Buton(){
+    return<Button variant= "outlined">Outlined</Button>;
+
+}
+
+export default Buton;
